1f7f4cb896ffaa21e356c7e7d8d4e56ecf33d40d
Adrian Oncioiu <106578989+adrianoncioiu@users.noreply.github.com>
2023-01-08 20:36:35 +0200

    08.01.23

--- procesare de img/main.py
+++ procesare de img/main.py
@@ -22,4 +22,49 @@ cv2.waitKey(4000)
 
 im = Image.open('poza_binary.jpg')
 pixels = list(im.getdata())
-print(pixels)
+
+ini = open("initial.txt", "w")
+f = open("results.txt", "w")
+
+j=0
+lin=0
+col=0
+mat = [[0 for _ in range(501)] for _ in range(501)]
+for i in pixels:
+    if i<50:
+        i = 1
+    else:
+        i = 0
+    f.write("%s " % i)
+
+    j+=1
+    lin = int(j/500)
+    col = j % 500
+
+    mat[lin][col] = i
+
+
+for i in range(501):
+    x1=0
+    y1=0
+    x2=0
+    y2=0
+    for j in range(501):
+        if mat[i][j] == 1:
+            if x1 == 0 and y1 == 0:
+                x1=i
+                y1=j
+            else:
+                x2=i
+                y2=j
+        if mat[i][j] == 0 and x1!=0 and y1 !=0 and x2 != 0 and y2 != 0:
+            print(x1, y1, "-", x2, y2, "\n")
+            ini.write("%s " % x1)
+            ini.write("%s " % y1)
+            ini.write("%s " % x2)
+            ini.write("%s " % y2)
+            ini.write("\n")
+            x1 = 0
+            y1 = 0
+            x2 = 0
+            y2 = 0
